311b7e7d8dc821456c99be3094dd5dad14c4151d
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2025-05-25 07:25:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

---
+++ leetcode-solutions/0957-minimum-add-to-make-parentheses-valid/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_cnt = 0
+        res = 0
+        for i in s:
+            if i == '(':
+                open_cnt +=1
+            else: 
+                open_cnt -=1
+                if open_cnt < 0:
+                    open_cnt = 0
+                    res+=1
+        return res + open_cnt
+
