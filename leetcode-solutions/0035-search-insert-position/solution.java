e9458717ed03670e2c4477065b755e03fd31fcaf
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2025-08-30 10:57:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (99.94%)

---
+++ leetcode-solutions/0035-search-insert-position/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return Search_insert_position(nums, target);
+    }
+
+    public static int Search_insert_position(int [] arr, int n) {
+		int res = 0;
+		for(int i = 0; i<arr.length; i++) {
+			if(n<=arr[i]) {
+				if(n==arr[i]) {
+					res = i;
+					break;
+				}
+				else {
+					res = i;
+					break;
+				}
+			}
+		}
+        if(n>arr[arr.length-1]){
+            res = arr.length;
+        }
+        if(n<arr[0]){
+            res = 0;
+        }
+		return res;
+		
+	}
+
+}
