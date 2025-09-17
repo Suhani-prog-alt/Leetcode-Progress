1e657cab1e24c542b5ec857423904bdd28235e7c
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2025-09-17 10:13:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (99.94%)

---
+++ leetcode-solutions/0031-next-permutation/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        permutation(nums);
+    }
+
+    public static void permutation(int[] arr) {
+		int p=-1;
+		for(int i=arr.length-2;i>=0;i--) {
+			if(arr[i]<arr[i+1]) {
+				p=i;
+				break;
+			}
+		}
+		if(p==-1) {
+			reverse(arr,0,arr.length-1);
+			return;
+		}
+		int q=-1;
+		for(int i=arr.length-1;i>p;i--) {
+			if(arr[i]>arr[p]) {
+				q=i;
+				break;
+			}
+		}
+		int temp=arr[p];
+		arr[p]=arr[q];
+		arr[q]=temp;
+		reverse(arr,p+1,arr.length-1);
+	}
+	public static void reverse(int[] arr, int i,int j) {
+		while(i<j) {
+			int temp=arr[i];
+			arr[i]=arr[j];
+		    arr[j]=temp;
+		    i++;
+		    j--;
+		}
+	}
+    
+}
