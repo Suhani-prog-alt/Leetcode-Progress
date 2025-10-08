c7e72a9a799746f1d47be4e8d06052d15289d2c8
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2025-10-08 17:48:50 +0000

    Sync LeetCode submission Runtime - 43 ms (97.52%), Memory - 64.9 MB (100.00%)

---
+++ leetcode-solutions/2392-successful-pairs-of-spells-and-potions/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int [] arr = new int [spells.length];
+        for(int i = 0; i<spells.length; i++){
+            int left = 0;
+            int right = potions.length;
+            while(left<right){
+                int mid = (left + right)/2;
+                if((long)spells[i]*potions[mid]>=success){
+                    right = mid;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            arr[i] = potions.length - left;
+        }
+        return arr;
+    }
+}
