09bead6d5679366aad5b22debdbb00d5a38b0657
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2025-11-12 10:22:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.1 MB (75.60%)

--- leetcode-solutions/0162-find-peak-element/solution.java
+++ leetcode-solutions/0162-find-peak-element/solution.java
@@ -1,13 +1,39 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int idx = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(max<nums[i]) {
-                idx = i;
-                max = nums[i];
-            }
-        }
-        return idx;
+// 
+        int lo  = 0, hi = nums.length-1, ans = nums.length -1;
+		while(lo<hi) {
+			int mid = (lo+hi)/2;
+			if(nums[mid]> nums[mid+1]) {
+				ans = mid;
+				hi = mid;
+			}else {
+				lo = mid+1;
+			}
+		}
+        return ans;
+
+
+        // linear search
+        // long max = Long.MIN_VALUE;
+		// int pi = -1;
+		// for(int i = 0; i<nums.length; i++) {
+		// 	if(nums[i]>max) {
+		// 		max = nums[i];
+		// 		pi = i;
+		// 	}
+		// }
+		// return pi;
+
+
+        // int max = Integer.MIN_VALUE;
+        // int idx = 0;
+        // for(int i = 0; i<nums.length; i++){
+        //     if(max<nums[i]) {
+        //         idx = i;
+        //         max = nums[i];
+        //     }
+        // }
+        // return idx;
     }
 }
