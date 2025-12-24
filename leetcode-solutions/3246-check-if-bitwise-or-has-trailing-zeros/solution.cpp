cc4e68382f83271a7b98391938d4ad5d971bee93
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2025-12-24 10:53:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 29 MB (50.38%)

---
+++ leetcode-solutions/3246-check-if-bitwise-or-has-trailing-zeros/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasTrailingZeros(vector<int>& nums) {
+        int count = 0;
+        for(int n : nums){
+            if(n%2==0){
+                count++;
+            }
+        }
+        if(count>=2) return true;
+        return false;
+    }
+};
