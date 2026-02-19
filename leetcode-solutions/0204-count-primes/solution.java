016aa0cd5c772a79a175b7aedcd477710c55b986
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-02-19 09:34:17 +0000

    Sync LeetCode submission Runtime - 102 ms (60.96%), Memory - 71.7 MB (6.69%)

---
+++ leetcode-solutions/0204-count-primes/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        return Prime_Sieve(n);
+    }
+    public static int Prime_Sieve(int n) {
+		int [] prime = new int [n];
+		prime[0] = prime[1] = 1;		// 0 and 1 is not prime number
+		for(int i = 2; i*i <= prime.length; i++	){
+			if( prime[i] == 0) {
+				for(int j =2; i*j< prime.length; j++) {
+					prime[i*j] = 1;
+				}
+			}
+		}
+		int count = 0;
+		for(int i = 2; i<prime.length; i++) {
+			if(prime[i] == 0) {
+				count++;
+			}
+		}
+        return count;
+	}
+}
