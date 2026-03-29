4920ca4dbe8f161a68c18aa72954ad26fc0fc596
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-03-29 02:36:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.5 MB (14.65%)

---
+++ leetcode-solutions/4264-first-matching-character-from-both-ends/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int ans = -1;
+        for(int i = 0; i<=s.length()/2; i++){
+            if(s.charAt(i) == s.charAt(s.length() - i- 1)) return i;
+        }
+        return ans;
+    }
+}
