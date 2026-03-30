7360f653ca9109e8d65046f42a1b9646e3594eb2
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-03-30 06:03:07 +0000

    Sync LeetCode submission Runtime - 1 ms (30.52%), Memory - 42.3 MB (66.75%)

--- leetcode-solutions/0191-number-of-1-bits/solution.java
+++ leetcode-solutions/0191-number-of-1-bits/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public int hammingWeight(int n) {
         int count = 0;
-        while(n>0){
-            int rem = n%2;
-            count+=rem;
-            n/=2;
+        int mask = 1;
+        for(int i = 0; i<32; i++){
+            if((n&mask) != 0){
+                count++;
+            }
+            mask<<=1;
         }
         return count;
     }
