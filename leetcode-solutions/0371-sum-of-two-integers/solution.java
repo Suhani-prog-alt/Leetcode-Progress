7f23800dbabeb994d6020f95dfbeb91a413ca674
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-04-07 02:18:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.8 MB (94.19%)

--- leetcode-solutions/0371-sum-of-two-integers/solution.java
+++ leetcode-solutions/0371-sum-of-two-integers/solution.java
@@ -1,5 +1,14 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a+b;
+        int xor = a^b;
+        int carry = a&b;
+        while(carry!=0){
+            carry<<=1;
+            a = xor;
+            b = carry;
+            xor = a^b;
+            carry = a&b;
+        }
+        return xor;
     }
 }
