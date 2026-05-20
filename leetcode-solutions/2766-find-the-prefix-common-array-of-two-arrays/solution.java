f208512b18760f087d9ec9f612b5d11003041001
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-05-20 09:38:19 +0000

    Sync LeetCode submission Runtime - 20 ms (9.52%), Memory - 47.4 MB (53.17%)

---
+++ leetcode-solutions/2766-find-the-prefix-common-array-of-two-arrays/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int l = A.length;
+        int [] c = new int [l];
+        for(int i = 0; i<l ; i++){
+            int count = 0;
+            for(int j = 0; j<=i; j++){
+                for(int k = 0; k<=i; k++){
+                    if(A[j]==B[k]) count++;
+                }
+            }
+            c[i] = count;
+        }
+        return c;
+    }
+}
