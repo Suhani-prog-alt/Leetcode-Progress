d48ddb518f31dcee94457e46fe07abff7040e785
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-05-23 15:35:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.1 MB (28.49%)

---
+++ leetcode-solutions/1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int c = 0;
+        int n = nums.length;
+        for(int i= 0; i<nums.length; i++){
+            if(nums[i]>nums[(i+1)%n])c++;
+        }
+        return c<=1;
+    }
+}
