c56661bd17875b74e0f855a6197c7afe482b6214
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-06-02 09:29:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (69.41%)

---
+++ leetcode-solutions/0395-longest-substring-with-at-least-k-repeating-characters/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        return help(s.toCharArray(),0, s.length(), k);
+    }
+
+    public static int help(char [] ch, int start, int end, int k){
+        if(end-start<k) return 0;
+        int [] count = new int [26];
+        for(int i = start; i<end; i++){
+            count[ch[i]-'a']++;
+        }
+        for(int i = start; i<end; i++){
+            if(count[ch[i]-'a']<k){
+                int j = i+1;
+
+                while(j<end && count[ch[j]-'a']<k){
+                    j++;
+                }
+                return Math.max(help(ch, start, i, k), help(ch, j, end,k));
+            }
+        }
+        return end-start;
+    }
+}
