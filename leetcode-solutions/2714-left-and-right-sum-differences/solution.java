c707a28e00c02db6b20808a898df56b6eec3e99e
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-06-06 17:33:11 +0000

    Sync LeetCode submission Runtime - 2 ms (99.06%), Memory - 46.5 MB (53.66%)

--- leetcode-solutions/2714-left-and-right-sum-differences/solution.java
+++ leetcode-solutions/2714-left-and-right-sum-differences/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        int [] left = new int [nums.length];
-        left[0] = 0;
+        int [] pre = new int [nums.length];
+        int [] suf = new int [nums.length];
+        
         for(int i = 1; i<nums.length; i++){
-            left[i] = left[i-1] + nums[i-1];
+            pre[i] = pre[i-1]+nums[i-1];
         }
-        int [] right = new int [nums.length];
-        right[nums.length-1] = 0;
         for(int i = nums.length-2; i>=0; i--){
-            right[i] = right[i+1] + nums[i+1];
+            suf[i] = suf[i+1]+nums[i+1];
         }
-
-        for(int i = 0; i<nums.length; i++){
-            nums[i] = Math.abs(left[i]-right[i]);
+        for(int i  = 0; i<nums.length; i++){
+            nums[i] = Math.abs(pre[i]-suf[i]);
         }
-
         return nums;
-
     }
 }
