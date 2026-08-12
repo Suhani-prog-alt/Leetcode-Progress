10420c561ba0b09b3affc140345b04c0f520389e
Suhani Sikarwar <204926493+Suhani-prog-alt@users.noreply.github.com>
2026-08-12 05:48:09 +0000

    Sync LeetCode submission Runtime - 2 ms (13.33%), Memory - 47 MB (74.44%)

--- leetcode-solutions/0543-diameter-of-binary-tree/solution.java
+++ leetcode-solutions/0543-diameter-of-binary-tree/solution.java
@@ -14,17 +14,23 @@
  * }
  */
 class Solution {
-    int diameter;
+    // 0 -  depth, 1 - diameter
     public int diameterOfBinaryTree(TreeNode root) {
-        diameter = 0;
-        depth(root);
-        return diameter;    
-}
-    public int depth (TreeNode node){
-        if(node ==null) return 0;
-        int L = depth(node.left);
-        int R = depth(node.right);
-        diameter = Math.max(diameter, L+R);
-        return Math.max(L,R)+1;
+        int [] arr = new int [2];
+        depth(root, arr);
+        return arr[1];
     }
+    public int [] depth(TreeNode root, int [] arr){
+        if(root  == null){
+            return new int []{0,0};
+        }
+        int [] left = depth(root.left, arr);
+        int [] right = depth(root.right, arr);
+
+        arr[1] = Math.max(arr[1], left[0]+right[0]);
+       
+
+        return new int []{Math.max(left[0],right[0])+1, arr[1]};
+    }
+
 }
